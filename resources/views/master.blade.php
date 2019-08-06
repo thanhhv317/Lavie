656aa4fa7527f266ddf50f2a154f00470e624c05
thanhhv317 <thanhhoang317@gmail.com>
2019-08-06 17:52:04 +0700

    share on fb

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -123,5 +123,26 @@
 </div>
 <!-- end footer page -->
 
+<!-- live chat talk to -->
+
+<!--Start of Tawk.to Script-->
+<!-- <script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5d494f557d27204601c98635/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script> -->
+<!--End of Tawk.to Script-->
+
+<!-- share to facebook -->
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v4.0&appId=528982404341906&autoLogAppEvents=1"></script>
+
+
 </body>
 </html>
